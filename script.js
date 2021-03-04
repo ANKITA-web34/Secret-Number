f07ffdb421172402b7d0b663704822adb278cc23
ANKITA-web34 <anki333soul@gmail.com>
2021-03-04 17:54:18 +0530

    refactoring the code

--- script.js
+++ script.js
@@ -43,7 +43,8 @@ document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector('.message').textContent = 'Start guessing...';
+  
+  displaMessage('Start guessing...')
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
